7e609c816b7693597d3a634a5b90c6d3b7eff4b4
Andrey <codenomnom@gmail.com>
2022-11-08 18:04:17 +0200

    chore: cleaning

--- .eleventy.js
+++ .eleventy.js
@@ -21,23 +21,6 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addCollection('pages', getPages);
 
-
-  // filtering (wip)
-  function filterTagList(tags) {
-    return (tags || []).filter(tag => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1);
-  }
-
-  eleventyConfig.addFilter('filterTagList', filterTagList)
-  eleventyConfig.addCollection('tagList', function(collection) {
-    let tagSet = new Set();
-    collection.getAll().forEach(item => {
-      (item.data.tags || []).forEach(tag => tagSet.add(tag));
-    });
-
-    const filtered = filterTagList([...tagSet]);
-    return filtered;
-  });
-
   return {
     markdownTemplateEngine: TEMPLATE_ENGINE,
     dataTemplateEngine: TEMPLATE_ENGINE,
