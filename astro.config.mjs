9047ba6a16b02c352911ac82f173c42a688e0606
Andrey <codenomnom@users.noreply.github.com>
2024-09-29 21:03:35 +0300

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-import tailwind from '@astrojs/tailwind';
+// import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [tailwind()]
+  site: 'https://quirks-mode.com',
+  integrations: [
+    // tailwind(),
+    sitemap(),
+  ]
 });
