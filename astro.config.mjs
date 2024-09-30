37a36df494b18a49309fce3ddd4dcf1ca22ca73a
Andrey <codenomnom@users.noreply.github.com>
2024-09-30 10:25:17 +0300

    feat: dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
 
 // import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://quirks-mode.com',
-  integrations: [
-    // tailwind(),
-    sitemap(),
-  ]
+  integrations: [// tailwind(),
+  sitemap(), icon()]
 });
