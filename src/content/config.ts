323cbc9f44015a1fc05cd7a57767b50fa3e448b3
Andrey <codenomnom@users.noreply.github.com>
2024-11-25 12:30:59 +0200

    fix: post/s plural in tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,25 @@
 import { defineCollection, getCollection, z, type CollectionEntry } from 'astro:content';
 // import { glob } from 'astro/loaders'; // not available with legacy API
 
+const schema = z.object({
+  title: z.string(),
+  date: z.coerce.date(),
+  description: z.string().optional(),
+  draft: z.boolean().optional(),
+  tags: z.array(z.string()).optional()
+});
+
 const quirks = defineCollection({
   type: 'content',
   // loader: glob({ pattern: '**\/[^_]*.md', base: './src/quirks' }),
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-    description: z.string().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).optional()
-  })
+  schema,
 });
 export const collections = { quirks };
 
+type Post = CollectionEntry<"quirks">;
+
 export async function getAllPosts() {
-  const posts = await getCollection("quirks", ({ data }) => {
+  const posts: Array<Post> = await getCollection("quirks", ({ data }: { data: z.infer<typeof schema> }) => {
     if (import.meta.env.PROD) { // production, so hide drafts and scheduled posts
       if (data.draft === true) {
         console.log(' > skipping draft article, as env is production (title: %s)', data.title);
@@ -40,11 +44,11 @@ export async function getAllPosts() {
   })
 }
 
-export async function getPostsGroupedByTags() {
+export async function getPostsGroupedByTags(): Promise<Map<string, Array<Post>>> {
   const posts = await getAllPosts();
   return posts.reduce(
-    (allTags: Map<string, Array<CollectionEntry<"quirks">>>, post) => {
-      const postTags = post.data.tags || [];
+    (allTags: Map<string, Array<Post>>, post: Post) => {
+      const postTags: Array<string> = post.data.tags || [];
       postTags.forEach((tag) => {
         if (!allTags.has(tag)) {
           allTags.set(tag, []);
